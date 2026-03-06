fd33720f6d458585ac18550be3826b50901c9fed
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-06 20:37:34 +0530

    read command

--- 05-read command.sh
+++ 05-read command.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Enter your PIN_NUMBER "
-read PIN_NUMBER    # HERE PIN NUMBER IS A VARIABLE
+read -s PIN_NUMBER    # HERE PIN NUMBER IS A VARIABLE
 echo "PIN is entered is: $PIN_NUMBER"
