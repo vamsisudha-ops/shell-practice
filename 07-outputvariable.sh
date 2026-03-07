a844c5ac1da4396adcf74b723da7e919eec4f760
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-07 14:38:57 +0530

    executed command

--- 07-outputvariable.sh
+++ 07-outputvariable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #DATE=$(date)
-START-TIME=$(date +%s)
+START_TIME=$(date +%s)
 sleep 10
-END-TIME=$(date +%s)
-TOTAL-TIME=$(($END-TIME-$START-TIME))
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "script executed in: $TOTAL_TIME"
