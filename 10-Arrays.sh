dd27d712e8bc5ebc68d20a46983a4416adc3d326
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-08 00:09:34 +0530

    arrays

--- 10-Arrays.sh
+++ 10-Arrays.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LEADERS=("Lakshman" "Kittu" "Prakash" "Akash")
 echo "All leaders: ${LEADERS[@]}"
-echo "First leader: ${LEADER[0]}"
+echo "First leader: ${LEADER[2]}"
 echo "First leader: ${LEADER[10]}"
