ff3b157ef47b23ca6b9a00973f7755a03c349968
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-07 23:32:57 +0530

    special variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,7 @@
 echo "all variables passed to the script: $@"
 echo "all variables passed to the script: $*"
 echo "name of the script: $0"
+echo "current directory: $PWD"
+echo "who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
