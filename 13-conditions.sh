ca2b4470b2180bf9e17974591224107911498196
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-08 18:00:17 +0530

    conditions

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -2,7 +2,7 @@
 echo "please enter the number:"
 read NUMBER
 
-if [ $(($NUMBER % 2)) -eq 0]; then 
+if [ $(($NUMBER % 2)) -eq 0 ]; then 
     echo "Given number $NUMBER is EVEN"
 else
     echo "Given number $NUMBER is ODD"
