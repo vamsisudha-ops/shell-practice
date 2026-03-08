9e61781b5f14e796b8f3aac170b11785e205bd80
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-08 18:07:46 +0530

    installations

--- 15-installations.sh
+++ 15-installations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-USERID= $(id -u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then 
     echo "ERROR:: please run this script with root previlege"
@@ -8,7 +8,7 @@ fi
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]; then    #validating either MySQL is installed or not based on previous command output 
     echo "ERROR: Installing MYSQL is failure"
     exit 1
 else
