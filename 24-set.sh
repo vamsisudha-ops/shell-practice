ae65fa09bc5ffdc64f2d31e702bb4a98d5614aeb
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-14 12:45:16 +0530

    set

--- 24-set.sh
+++ 24-set.sh
@@ -1,6 +1,13 @@
 #!/bin/bash 
-set -e 
+set -e   ## e-exit, immediately exit if error occurs
+
+error(){
+        echo "There is an error"
+}
+
+trap error ERR
+
 echo "Hello .."
 echo "Before error.."
-sfdgsdfg
+sfdgsdfg    ## here shell understands there is an error and signal is ERR 
 echo "After error.."
