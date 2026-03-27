f72cb61dc8f31513bfaae9e5d14ac8c250063b9b
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-27 15:01:28 +0530

    back up

--- 29-backup.sh
+++ 29-backup.sh
@@ -4,6 +4,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
@@ -25,3 +27,13 @@ USAGE(){
 if [ $# -lt 2 ]; then
     USAGE
 fi 
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo -e "$R $SOURCE_DIR does not exist $N"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]; then
+    echo -e "$R $DEST_DIR does not exist $N"
+    exit 1
+fi
