cbaf6c9efdc058a783b8e4a462ac543a35a147b4
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-27 17:44:51 +0530

    source

--- 25-script-1.sh
+++ 25-script-1.sh
@@ -3,6 +3,6 @@
 NAME=INDIA
 echo "My Country: $NAME"
 echo "PID of SCRIPT-1: $$" # $$ to get PID of the instance
-sh 26-script-2.sh
+#sh 26-script-2.sh
 
-source /26-script-2.sh
+source ./26-script-2.sh
