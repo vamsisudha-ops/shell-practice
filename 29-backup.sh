f133d37bcb95611faa1561ab8d95a22587b6f6cd
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-27 14:53:22 +0530

    back up

--- 29-backup.sh
+++ 29-backup.sh
@@ -18,7 +18,7 @@ if [ $USERID -ne 0 ]; then
 fi 
 
 USAGE(){
-    echo "USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days]"
+    echo -e "$R USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days] $N"
     exit 1
 }
 
