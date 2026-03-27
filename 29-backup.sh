4e1f736d396373f0421414ee2b966c0be0e9de83
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-27 14:52:02 +0530

    back up

--- 29-backup.sh
+++ 29-backup.sh
@@ -12,18 +12,16 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log  --
 mkdir -p $LOGS_FOLDER   # -p checks already directory is there or not, if it's not automatically it create a directory
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE  #TO APPEND THE ECHO STATEMENT IN LOGS FILE
 
-SOURCE_DIR=/home/ec2-user/app-logs 
+if [ $USERID -ne 0 ]; then 
+    echo "ERROR:: please run this script with root previlege"
+    exit 1 #failure is other than 0 
+fi 
 
-if [ ! -d $SOURCE_DIR ]; then
-    echo -e "ERROR:: $SOURCE_DIR does not exit"
+USAGE(){
+    echo "USAGE:: sudo sh 24-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS>[optional, default 14 days]"
     exit 1
-fi
+}
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
-
-while IFS= read -r filepath
-do
-    echo "Deleting the file: $filepath"
-    rm -rf $filepath 
-    echo "Deleted the file: $filepath"
-done <<< $FILES_TO_DELETE
+if [ $# -lt 2 ]; then
+    USAGE
+fi 
