09bf5a08b7c3a25270f52f221301584fc27bcfbc
vamsisudha-ops <vamsisudha49@gmail.com>
2026-03-27 11:10:01 +0530

    delete old log files

--- 27-delete-old-logs.sh
+++ 27-delete-old-logs.sh
@@ -20,3 +20,8 @@ if [ ! -d $SOURCE_DIR ]; then
 fi
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+
+while IFS= read -r filepath
+do
+    echo "Deleting the file: $filepath"
+done <<< $FILES_TO_DELETE
